643ab10e6f1e59df4c5a357537b43c47950cb014
MatheusGothe <matheus.gothe@bs.nttdata.com>
2025-10-05 15:54:38 -0300

    Ajustes de biliotecas

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,6 +1,5 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Historico from "./pages/Historico";
